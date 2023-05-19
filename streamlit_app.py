03ecede0a8f9fef937cab4a815ff86bc9db139ba
Estevefact <132919839+Estevefact@users.noreply.github.com>
2023-05-19 09:15:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-stremalit.title('My parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kael, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Eggs')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kael, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Eggs')
+streamlit.text('🥑🍞 HAvocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
